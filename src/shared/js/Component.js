ea60e6e9b7d3c01988c3bbb7172d5eb3bd750b12
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-08-25 19:50:15 -0300

    Just testing workflow

---
+++ src/shared/js/Component.js
@@ -0,0 +1,14 @@
+/**
+ * Represents an component in my architecture
+ */
+export class Component
+{
+    /**
+     * @returns { HTMLElement } the html page
+     */
+    render()
+    {
+        throw new Error("Method not Implemented");
+    }
+}
+
